99a7819011337cb90e7b3f9151a4ba4f05fc18b5
Your Name <you@example.com>
2025-11-02 10:55:30 +0530

    more improvements

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <div className="flex items-center justify-center h-full">
-      <div className="w-10 h-10 border-4 border-rose-400 border-t-transparent rounded-full animate-spin"></div>
+      <div className="w-10 h-10 border-4 border-(--color-primary) border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 };
